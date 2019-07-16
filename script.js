b0c545583f290776b5cdd4e65b617d582c953030
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-15 22:01:01 -0300

    Adicionar exercício de for in

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+Crie um sistema que apresenta uma lista de gostos do usuário. O sistema deve:
+- Perguntar se o usuário quer adicionar um novo item na lista;
+- Adicionar a entrada do usuário em uma lista de gostos;
+Assim que o usuário não quiser mais adicionar um novo item na lista, você deve mostrar cada um dos itens da lista seguindo o esquema:
+"Meu gosto nº 1 é ..."
+"Meu gosto nº 2 é ..."
+"Meu gosto nº 3 é ..."
+*/
