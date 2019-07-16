eed8e29d1cec7cfbc078581d0062e957ee084d65
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-16 19:36:05 -0300

    Adicionar solução do exercício de for in

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ Assim que o usuário não quiser mais adicionar um novo item na lista, você dev
 "Meu gosto nº 2 é ..."
 "Meu gosto nº 3 é ..."
 */
+let deveAdicionar = confirm('Você deseja adicionar um novo gosto à lista?');
+let lista = [];
+
+
+while (deveAdicionar) {
+    let gosto = prompt('O que você gosta?');
+    lista.push(gosto);
+
+    deveAdicionar = confirm('Você deseja adicionar um novo gosto à lista?');
+}
+
+// let i = 0;
+// while (i < lista.length) {
+//     let item = lista[i];
+//     console.log(`Meu gosto nº ${i+1} é ${item}`);
+//     i++;
+// }
+
+for(let indice in lista) {
+    let gosto = lista[indice];
+    console.log(`Meu gosto nº ${Number(indice)+1} é ${gosto}`);
+}
